9f226d22951baefe1474217d8834f37976dd6936
AsianJohnBoi <Jbssantias@hotmail.com>
2020-08-03 19:47:31 +1000

    env variables

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
+require('dotenv').config();
+
 const puppeteer = require('puppeteer');
 
 (async () => {
     const browser = await puppeteer.launch({headless: false});
     const page = await browser.newPage();
-    await page.goto('https://google.com');
+    await page.goto('https://stake.com.au/');
 
+    await page.type("#input_9", process.env.STAKE_USER);
+    await page.type("#input_10", process.env.STAKE_PASS);
+    await page.keyboard.press("Enter")
     //   await browser.close();
 })();
